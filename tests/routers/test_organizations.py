a4a78d5cd783cbcc5a08ac4f457ecdb819e8d2b7
Xinyi Rong <Lizard.rar@gmail.com>
2025-04-23 16:56:31 -0700

    test: add tests for pagination

--- tests/routers/test_organizations.py
+++ tests/routers/test_organizations.py
@@ -1,3 +1,8 @@
+from itertools import chain
+from operator import attrgetter
+from typing import Any
+
+import pytest
 from faker import Faker
 from httpx import AsyncClient
 
@@ -24,3 +29,58 @@ class TestListOrganizations:
             ],
             "next_cursor": None,
         }
+
+
+@uses_db
+class TestPagination:
+    url = "/organizations/"
+
+    @pytest.fixture
+    async def organizations(self, faker: Faker) -> list[Organization]:
+        organizations = [
+            await Organization.create(name=faker.company()) for _ in range(20)
+        ]
+        organizations.sort(key=attrgetter("id"), reverse=True)
+        return organizations
+
+    @pytest.mark.parametrize("per_page", [1, 3, 5, 9, 11, 50])
+    async def test_all_pages(
+        self,
+        client: AsyncClient,
+        organizations: list[Organization],
+        per_page: int,
+    ) -> None:
+        pages: list[list[dict[str, Any]]] = []
+        params = {"limit": per_page}
+        while True:
+            response = await client.get(self.url, params=params)
+            assert response.status_code == 200
+            data = response.json()
+
+            pages.append(data["items"])
+
+            next_cursor = data["next_cursor"]
+            if not next_cursor:
+                break
+            params["cursor"] = next_cursor
+
+        assert all(len(page) == per_page for page in pages[:-1])
+        assert len(pages[-1]) <= per_page
+
+        for obj, organization in zip(
+            chain.from_iterable(pages),
+            organizations,
+            strict=True,
+        ):
+            assert obj["id"] == organization.id
+            assert obj["name"] == organization.name
+
+    @pytest.mark.parametrize("cursor", ["", "foobar", "1" * 1000])
+    async def test_cursor_invalid(self, client: AsyncClient, cursor: str) -> None:
+        response = await client.get(self.url, params={"cursor": cursor})
+        assert response.status_code == 422
+
+    @pytest.mark.parametrize("limit", ["", "foobar", "-1", "10000", "1" * 1000])
+    async def test_limit_invalid(self, client: AsyncClient, limit: str) -> None:
+        response = await client.get(self.url, params={"limit": limit})
+        assert response.status_code == 422
